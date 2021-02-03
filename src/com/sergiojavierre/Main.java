344fe0a717b7a87c93dcb6bfbcf3a5cf73725746
Gabriel <gsanchezrodrigo@gmail.com>
2021-02-03 10:37:56 +0100

    Prueba contador

--- src/com/sergiojavierre/Main.java
+++ src/com/sergiojavierre/Main.java
@@ -5,6 +5,7 @@ import com.sergiojavierre.lector.LectorTXT;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class Main {
@@ -13,13 +14,22 @@ public class Main {
         // write your code here
         Lector lector = new LectorTXT();
         String texto = lector.lee("el_quijote.txt");
-        List<String> caracteres = new ArrayList<>(List.of(".",",",";",":","-","?","¿","!","¡","(",")"));
+        List<String> caracteres = new ArrayList<>(List.of(".",",",";",":","–","?","¿","!","¡","(",")"));
         for (String caracter:caracteres){
             texto = texto.replace(caracter,"");
         }
+
         String[] separar = texto.split(" ");
+        HashMap<String, Integer> mapaPalabras = new HashMap<>(separar.length);
         for (int i = 0; i < separar.length; i++) {
-            System.out.println(separar[i]);
+            //System.out.println(separar[i]);
+            if (mapaPalabras.containsKey(separar[i])){
+                mapaPalabras.put(separar[i], mapaPalabras.get(separar[i]) + 1);
+
+            }else {
+                mapaPalabras.put(separar[i],1);
+            }
         }
+        System.out.println(mapaPalabras);
     }
 }
