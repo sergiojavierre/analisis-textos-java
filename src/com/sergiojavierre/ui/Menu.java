2d6f4600e4beec8c51edfc35968b4547c40b6e32
estudiante <jorgemarcoaltabas@gmail.com>
2021-02-03 12:21:44 +0100

    Arreglo menu

--- src/com/sergiojavierre/ui/Menu.java
+++ src/com/sergiojavierre/ui/Menu.java
@@ -1,20 +1,26 @@
 package com.sergiojavierre.ui;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Menu {
     public static void menuPrincipal(){
         Scanner sc = new Scanner(System.in);
         while (true){
-            System.out.println("1- Escoge un atajo ");
+            System.out.println("1- Escoge un atajo\nOpcion: ");
             String opcion = sc.nextLine();
             if (opcion.equals("1")){
-                System.out.println("A) Numero de palabras");
-                System.out.println("B) Palabras relacionadas");
-                if (opcion.equals("A")){
+                System.out.println("A) Numero de palabras\nB) Palabras relacionadas\nC) Salir\nOpcion: ");
+                String opcion2 = sc.nextLine();
+                opcion2 = opcion2.toUpperCase(Locale.ROOT);
+                if (opcion2.equals("A")){
 
                 }
-                else if (opcion.equals("B")){
+                else if (opcion2.equals("B")){
+                }
+
+                else if (opcion2.equals("C")){
+                    break;
                 }
             }
         }
