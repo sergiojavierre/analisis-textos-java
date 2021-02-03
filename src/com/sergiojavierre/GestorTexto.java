c49459658d50ed783f345d6f992101f910c994c0
ByChunai <unai64535@gmail.com>
2021-02-03 13:46:14 +0100

    Menu version 1.0

--- src/com/sergiojavierre/GestorTexto.java
+++ src/com/sergiojavierre/GestorTexto.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class GestorTexto {
     private String texto;
     LinkedHashMap<String, Integer> mapaPalabras = new LinkedHashMap<>();
+    LinkedHashMap<String, Integer> contadorPalabras = new LinkedHashMap<>();
     String[] palabras = new String[]{};
 
 
@@ -13,35 +14,54 @@ public class GestorTexto {
         separarPalabras();
     }
 
-    private void separarPalabras(){
+    private void separarPalabras() {
         texto = texto.toLowerCase(Locale.ROOT);
-        List<String> caracteres = new ArrayList<>(List.of(".",",",";",":","–","?","¿","!","¡","(",")","\n","{","}","-","»","«"));
-        for (String caracter:caracteres){
-            texto = texto.replace(caracter,"");
+        List<String> caracteres = new ArrayList<>(List.of(".", ",", ";", ":", "–", "?", "¿", "!", "¡", "(", ")", "\n", "{", "}", "-", "»", "«"));
+        for (String caracter : caracteres) {
+            texto = texto.replace(caracter, "");
         }
         palabras = texto.split(" ");
         for (int i = 0; i < palabras.length; i++) {
-            if (mapaPalabras.containsKey(palabras[i])){
+            if (mapaPalabras.containsKey(palabras[i])) {
                 mapaPalabras.put(palabras[i], mapaPalabras.get(palabras[i]) + 1);
 
-            }else {
-                mapaPalabras.put(palabras[i],1);
+            } else {
+                mapaPalabras.put(palabras[i], 1);
             }
         }
     }
 
-    public void contarTodasPalabras(){
-        for (Object key:mapaPalabras.keySet()){
-            System.out.println(key+" "+mapaPalabras.get(key));
+    public void contarTodasPalabras() {
+        for (Object key : mapaPalabras.keySet()) {
+            System.out.println(key + " " + mapaPalabras.get(key));
         }
     }
 
-    public void buscarpalabra(String palabra){
-        if (mapaPalabras.containsKey(palabra)){
-            System.out.println(mapaPalabras);
-            System.out.println(palabra+" "+mapaPalabras.get(palabra));
-        }
-        else System.out.println("la palabra: "+palabra+", no existe");
-    }
+    public void buscarpalabra(String palabra) {
+        for (int i = 0; i < palabras.length; i++) {
+            if (palabras[i].equals(palabra)){
+                String antes = null, despues = null;
+                if(i > 0){
+                    antes = palabras[i-1];
+                }
+                if(i < palabras.length-1){
+                    despues = palabras[i+1];
+                }
 
+                System.out.println(antes+", "+palabra+", "+despues);
+
+                if (antes != null) {
+                    Integer veces = contadorPalabras.get(antes);
+                    if(veces == null) veces = 0;
+                    contadorPalabras.put(antes, veces + 1);
+                }
+                if (despues != null){
+                    Integer veces = contadorPalabras.get(despues);
+                    if(veces == null) veces = 0;
+                    contadorPalabras.put(despues, veces + 1);
+                }
+
+            }
+        }
+    }
 }
