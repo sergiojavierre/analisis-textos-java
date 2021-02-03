b85acbce17a69202ecd4f14d6def760a0f6d34fb
ByChunai <unai64535@gmail.com>
2021-02-03 12:37:09 +0100

    Menu estable

--- src/com/sergiojavierre/GestorTexto.java
+++ src/com/sergiojavierre/GestorTexto.java
@@ -20,14 +20,7 @@ public class GestorTexto {
             texto = texto.replace(caracter,"");
         }
         palabras = texto.split(" ");
-        contarTodasPalabras();
-        buscarpalabra();
-    }
-
-    public void contarTodasPalabras(){
-
         for (int i = 0; i < palabras.length; i++) {
-            //System.out.println(separar[i]);
             if (mapaPalabras.containsKey(palabras[i])){
                 mapaPalabras.put(palabras[i], mapaPalabras.get(palabras[i]) + 1);
 
@@ -35,19 +28,20 @@ public class GestorTexto {
                 mapaPalabras.put(palabras[i],1);
             }
         }
-/*
+    }
+
+    public void contarTodasPalabras(){
         for (Object key:mapaPalabras.keySet()){
             System.out.println(key+" "+mapaPalabras.get(key));
         }
-
- */
     }
 
-    public void buscarpalabra(){
-        String palabra = "d";
+    public void buscarpalabra(String palabra){
         if (mapaPalabras.containsKey(palabra)){
+            System.out.println(mapaPalabras);
             System.out.println(palabra+" "+mapaPalabras.get(palabra));
         }
+        else System.out.println("la palabra: "+palabra+", no existe");
     }
 
 }
--- src/com/sergiojavierre/ui/Menu.java
+++ src/com/sergiojavierre/ui/Menu.java
@@ -13,9 +13,7 @@ public class Menu {
         String texto = lector.lee("el_quijote.txt");
         GestorTexto gestorTexto = new GestorTexto(texto);
         while (true){
-            System.out.println("1- Escoge un atajo\nOpcion: ");
-            String opcion = sc.nextLine();
-            if (opcion.equals("1")){
+
                 System.out.println("A) Numero de palabras\nB) Palabras relacionadas\nC) Salir\nOpcion: ");
                 String opcion2 = sc.nextLine();
                 opcion2 = opcion2.toUpperCase(Locale.ROOT);
@@ -24,12 +22,13 @@ public class Menu {
 
                 }
                 else if (opcion2.equals("B")){
-                    gestorTexto.buscarpalabra();
+                    System.out.print("Elije una palabra a buscar\nOpcion:");
+                    String palabra = sc.nextLine();
+                    gestorTexto.buscarpalabra(palabra);
                 }
                 else if (opcion2.equals("C")){
                     break;
                 }
             }
         }
-    }
 }
