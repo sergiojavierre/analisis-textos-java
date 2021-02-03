6b61d00445e862de7f706eab30be85aea8668bb7
estudiante <jorgemarcoaltabas@gmail.com>
2021-02-03 12:11:42 +0100

    Creada UI

--- src/com/sergiojavierre/Main.java
+++ src/com/sergiojavierre/Main.java
@@ -1,36 +1,15 @@
 package com.sergiojavierre;
 
-import com.sergiojavierre.lector.Lector;
-import com.sergiojavierre.lector.LectorTXT;
+import com.sergiojavierre.ui.Menu;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
-        Lector lector = new LectorTXT();
-        String texto = lector.lee("el_quijote.txt");
-        List<String> caracteres = new ArrayList<>(List.of(".",",",";",":","–","?","¿","!","¡","(",")"));
-        for (String caracter:caracteres){
-            texto = texto.replace(caracter,"");
-        }
 
-        String[] separar = texto.split(" ");
-        HashMap<String, Integer> mapaPalabras = new HashMap<>(separar.length);
-        for (int i = 0; i < separar.length; i++) {
-            //System.out.println(separar[i]);
-            if (mapaPalabras.containsKey(separar[i])){
-                mapaPalabras.put(separar[i], mapaPalabras.get(separar[i]) + 1);
+        Menu menu = new Menu();
+        menu.menuPrincipal();
 
-            }else {
-                mapaPalabras.put(separar[i],1);
-            }
-        }
-        System.out.println(mapaPalabras);
 
     }
 }
--- src/com/sergiojavierre/ui/Menu.java
+++ src/com/sergiojavierre/ui/Menu.java
@@ -1,4 +1,22 @@
 package com.sergiojavierre.ui;
 
+import java.util.Scanner;
+
 public class Menu {
+    public static void menuPrincipal(){
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            System.out.println("1- Escoge un atajo ");
+            String opcion = sc.nextLine();
+            if (opcion.equals("1")){
+                System.out.println("A) Numero de palabras");
+                System.out.println("B) Palabras relacionadas");
+                if (opcion.equals("A")){
+
+                }
+                else if (opcion.equals("B")){
+                }
+            }
+        }
+    }
 }
