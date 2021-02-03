fe09a54fa7b983f99ae95e01ba34ef21e0aa5bbf
Gabriel <gsanchezrodrigo@gmail.com>
2021-02-03 10:00:34 +0100

    Borrar caracteres

--- src/com/sergiojavierre/Main.java
+++ src/com/sergiojavierre/Main.java
@@ -3,15 +3,23 @@ package com.sergiojavierre;
 import com.sergiojavierre.lector.Lector;
 import com.sergiojavierre.lector.LectorTXT;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         Lector lector = new LectorTXT();
         String texto = lector.lee("el_quijote.txt");
-        String [] palabras = texto.split(" ");
-        for (int i = 0; i < palabras.length; i++) {
-            System.out.println(palabras[i]);
+        List<String> caracteres = new ArrayList<>(List.of(".",",",";",":","-","?","¿","!","¡","(",")"));
+        for (String caracter:caracteres){
+            texto = texto.replace(caracter,"");
+        }
+        String[] separar = texto.split(" ");
+        for (int i = 0; i < separar.length; i++) {
+            System.out.println(separar[i]);
         }
     }
 }
